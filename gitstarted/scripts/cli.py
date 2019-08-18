db39319a85601639fa54a13e69b74d81acdd5188
Affan Ahmed <affan36@outlook.com>
2019-08-17 22:03:25 -0400

    Changed folder to src

--- gitstarted/scripts/cli.py
+++
@@ -1,9 +0,0 @@
-"""
-Python command line script to automate git project creation
-"""
-import click
-
-@click.command()
-@click.option('--name', required=True, prompt=True, help="Provide a name for your repo")
-def cli(name):
-    click.echo(name)
